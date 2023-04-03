25eb800a6d1fddeb477451f9249f766ff4e9b68e
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2023-04-03 10:50:45 +0200

    Add project files.

---
+++ Slutprojekt_Prog2/Kund.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slutprojekt_Prog2
+{
+    class Kund
+    {
+        int antalpersoner;
+        int tid;
+        int datum;
+       
+        
+        public Kund()
+        {
+            Console.WriteLine("Hur stort sällskap är ni?");
+            this.antalpersoner = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken tid tänkte ni komma?");
+
+        }
+    }
+}
